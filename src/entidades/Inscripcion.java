422837c3a7b6eda9a61bf50893c8cdf470421c6a
Verocgreco <veronicacgreco@outlook.com>
2023-09-25 11:14:32 -0300

    Initial commit

---
+++ src/entidades/Inscripcion.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author arielvallejos
+ */
+public class Inscripcion {
+    
+    private int idInscripto;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idInscripto = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion() {
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno: " + alumno + ", Materia: " + materia + ", Nota=" + nota;
+    }
+    
+    
+    
+ 
+    
+    
+}
---
+++ src/ulpgrupo48proyectotransversal/ULPGrupo48ProyectoTransversal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ulpgrupo48proyectotransversal;
+
+/**
+ *
+ * @author Grupo 48
+ */
+public class ULPGrupo48ProyectoTransversal {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
